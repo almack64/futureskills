63352d63fe49976830e51bd2fe91168465950f34
almack64 <41652805+almack64@users.noreply.github.com>
2025-01-23 14:58:53 -0600

    Create mailchimp.js
    Mail Chimp Verfication added

---
+++ mailchimp.js
@@ -0,0 +1,20 @@
+<script type="text/javascript" src="//s3.amazonaws.com/downloads.mailchimp.com/js/mc-validate.js"></script>
+<script type="text/javascript">
+    (function($) {
+        window.fnames = new Array();
+        window.ftypes = new Array();
+        fnames[0] = 'EMAIL';
+        ftypes[0] = 'email';
+        fnames[1] = 'FNAME';
+        ftypes[1] = 'text';
+        fnames[2] = 'LNAME';
+        ftypes[2] = 'text';
+        fnames[3] = 'PHONE';
+        ftypes[3] = 'phone';
+        fnames[4] = 'COMPANY';
+        ftypes[4] = 'text';
+        fnames[5] = 'MMERGE7';
+        ftypes[5] = 'radio';
+    }(jQuery));
+    var $mcj = jQuery.noConflict(true);
+</script>
