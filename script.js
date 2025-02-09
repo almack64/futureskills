f327ccacae71f876fa5b7478ee34f61ee11f9a83
almack64 <41652805+almack64@users.noreply.github.com>
2025-02-09 14:27:54 -0600

    Update script.js

--- script.js
+++ script.js
@@ -178,3 +178,29 @@ window.addEventListener('scroll', () => {
         header.style.transform = 'translateY(0)';
     }
 });
+// Set the date we're counting down to (Feb 18, 2025, 6:30 PM CST)
+var countDownDate = new Date("Feb 18, 2025 18:30:00 CST").getTime();
+
+// Update the count down every 1 second
+var countdownInterval = setInterval(function() {
+  // Get today's date and time
+  var now = new Date().getTime();
+  
+  // Find the distance between now and the count down date
+  var distance = countDownDate - now;
+  
+  // Time calculations for days, hours, minutes and seconds
+  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+  
+  // Display the result in the element with id="countdown-timer"
+  document.getElementById("countdown-timer").innerHTML = days + "d " + hours + "h " + minutes + "m " + seconds + "s ";
+  
+  // If the count down is finished, write some text
+  if (distance < 0) {
+    clearInterval(countdownInterval);
+    document.getElementById("countdown-timer").innerHTML = "The Success Lab has begun!";
+  }
+}, 1000);
